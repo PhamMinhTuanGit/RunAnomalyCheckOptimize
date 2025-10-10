5ac0c98358ae6d6075a0ba44cee7c36488096af4
PhamMinhTuanGit <mtgaming200417@gmail.com>
2025-10-10 20:57:25 +0700

    update remote to match my device ver

--- test_anomaly_segment.py
+++ test_anomaly_segment.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from collections import defaultdict
 
 # === CONFIGURATION ===
-csv_path = "results/prediction_nhits_7.71.csv"             # 📝 Replace with your CSV file
+csv_path = "results/prediction_timesnet_18.75.csv"             # 📝 Replace with your CSV file
 timestamp_col = "ds"                   # Name of the timestamp column
 segment_length = 12
 
@@ -13,9 +13,9 @@ segment_length = 12
 y_col = 0
 y_pred_col = 5
 lo_95_col = 6
-hi_95_col = 9
+hi_95_col = 15
 
-output_dir = "results/anomaly_segemnts_nhits_7.7M_interval90"
+output_dir = "results/anomaly_segemnts_timesnet_18.75M_interval90"
 os.makedirs(output_dir, exist_ok=True)
 
 # === 1. Load CSV safely ===
